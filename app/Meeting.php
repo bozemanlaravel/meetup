83896b4825dbe96c545c273b37dc9f60f02bd2e0
Greg Leman <gleman17@gmail.com>
2019-10-03 14:14:20 -0600

    Added users hasmanythrough relation on Meeting
    Added service addUserToMeeting and tests

--- app/Meeting.php
+++ app/Meeting.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Meeting extends Model
 {
     protected $guarded = [];
+
+    /**
+     * @return HasManyThrough
+     */
+    public function users() : HasManyThrough
+    {
+        return $this->hasManyThrough(User::class,MeetingUser::class,
+            'user_id', 'id');
+    }
 }
--- app/Services/MeetingUserService.php
+++ app/Services/MeetingUserService.php
@@ -13,8 +13,14 @@ use App\User;
 
 class MeetingUserService
 {
-    public function addUserToMeeting(User $user, Meeting $meeting, boolean $attending) : MeetingUser
+    public function addUserToMeeting(User $user, Meeting $meeting, bool $attending) : MeetingUser
     {
+        $meeting_user = MeetingUser::create([
+            'user_id' => $user->id,
+            'meeting_id' => $meeting->id,
+            'attending' => $attending
+        ]);
 
+        return $meeting_user;
     }
 }
