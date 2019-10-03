653e4fa12a499bca152d5cf184df3d9d5c9ccc03
Dustin Fraker <drfraker@gmail.com>
2019-10-02 21:19:02 -0600

    add some meeting stuff

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('meetings/create', 'MeetingsController@create')->name('meetings.create');
+    Route::post('meetings', 'MeetingsController@store')->name('meetings.store');
+});
+
