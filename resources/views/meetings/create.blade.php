6539f01798bbfc0f2301d1561ae97644d686ecfa
Joe Sweeney <joe.sweeney224@gmail.com>
2019-10-07 15:51:02 -0600

    Something to show in create meeting view

---
+++ resources/views/meetings/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex items-center">
+        <div class="md:w-1/2 md:mx-auto">
+
+            @if (session('status'))
+                <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            <div>Create a new meeting</div>
+        </div>
+    </div>
+@endsection
