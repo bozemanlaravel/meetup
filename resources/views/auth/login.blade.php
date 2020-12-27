c78bb5d90b89b0d46f2a3b819faaf828b8803f56
Joe Sweeney <joe.sweeney224@gmail.com>
2020-12-26 18:01:41 -0700

    mobile fixes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container mx-auto">
         <div class="flex flex-wrap justify-center">
-            <div class="w-full max-w-sm">
+            <div class="w-full max-w-sm px-3">
                 <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
 
                     <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
