32e22ba85a4d4cb6008e258679cf7d36e953859a
Joe Sweeney <joe.sweeney224@gmail.com>
2021-03-08 19:11:27 -0700

    jazz up homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,25 +13,26 @@
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="bg-gray-100 h-screen antialiased leading-none">
-<div class="flex flex-col">
-    @if(Route::has('login'))
-        <div class="absolute top-0 right-0 mt-4 mr-4">
-            @auth
-                <a href="{{ url('/home') }}" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase">{{ __('Home') }}</a>
-            @else
-                <a href="{{ route('login') }}" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase pr-6">{{ __('Login') }}</a>
-            @endauth
-        </div>
-    @endif
+<body class="bg-gradient-to-r from-red-500 to-pink-500 h-screen antialiased leading-none text-white">
+    <div class="min-h-screen flex flex-col">
+        @if(Route::has('login'))
+            <div class="bg-black bg-opacity-25 p-4 flex justify-end">
+                @auth
+                    <a href="{{ url('/home') }}" class="hover:underline text-sm font-normal uppercase">{{ __('Home') }}</a>
+                @else
+                    <a href="{{ route('login') }}" class="hover:underline text-sm font-normal uppercase pr-6">{{ __('Login') }}</a>
+                @endauth
+            </div>
+        @endif
 
-    <div class="min-h-screen flex items-center justify-center">
-        <div class="flex flex-col justify-around h-full">
-           <div>
-               <a class="underline" href="/register">Join Us</a>
-           </div>
+        <div class="flex-1 flex items-center justify-center p-4">
+            <div class="flex flex-col justify-around h-full">
+            <div class="text-center">
+                <h1 class="text-6xl">Welcome to {{ config('meetup.site_name') }}</h1>
+                <a class="inline-block mt-10 text-4xl bg-white text-red-500 rounded p-3" href="/register">Join Us</a>
+            </div>
+            </div>
         </div>
     </div>
-</div>
 </body>
 </html>
