599d74256cc2f84a9e1687dcf1279b753e5c68c4
Joe Sweeney <joe.sweeney224@gmail.com>
2021-04-01 19:49:32 -0600

    force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,9 @@ namespace App\Providers;
 use App\User;
 use App\Services\RoleService;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (App::environment() == 'production') {
+            //URL::forceRootUrl(config('app.url'));
+            URL::forceScheme('https');
+        }
     }
 }
